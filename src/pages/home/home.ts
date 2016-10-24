751c6032ef78986d24100dd0bbd872278ccff936
Sumit-PC\Sumit <sumitpathak83@gmail.com>
2016-10-25 00:36:01 +0530

    Updated half pages..some trouble with www directory. Holding off further checkin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { SignUpPage } from '../pages/signup/signup';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,13 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+	  
+ <ion-content>
+  <ion-list>
+    <button block ion-button (click)="openSignUpPage()" icon-left>
+    </button>
+  </ion-list>
+</ion-content>
     
   }
 
